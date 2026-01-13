ffd3146de6937ff6133af71616844dff20e69435
AlfonsoMakajumba <juliusz129@gmail.com>
2026-01-13 17:36:21 +0100

    giga baza

---
+++ console_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct cwel
+{
+    int chuj;
+    int pizda;
+    char knur;
+    char chujnia;
+    char asd;
+    char asdd;
+};
+
+int main()
+{
+    printf("Size of struct: %zu bytes\n", sizeof(struct cwel));
+    return 0;
+}
+
